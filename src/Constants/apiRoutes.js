4945470ba65b4092455a16687ea23eb00ebf71cf
Nourelden Rihan <noureldenrihan@gmail.com>
2024-04-13 16:59:04 +0200

    Updated API Route to use Production Backend Host Address

--- src/Constants/apiRoutes.js
+++ src/Constants/apiRoutes.js
@@ -1,10 +1,12 @@
 // API Host and Routes Grouper
 
-const hostAddress = "http://localhost:5000"; // Needs to be Updated before Production
+const hostAddress = "https://spire-treasury-backend.onrender.com"; // Production Backend Host Address
+const devAddress = "http://localhost:5000"; // Development Backend Host Address
 
 const apiRoutes = {
   createUser: hostAddress + "/users/signup",
   loginUser: hostAddress + "/users/login",
+  getSingleUser: hostAddress + "/users/user",
 };
 
 export default apiRoutes;
