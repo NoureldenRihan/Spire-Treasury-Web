f23b6b8f0d910fe9316945cc514582187b12e2e4
Nourelden Rihan <noureldenrihan@gmail.com>
2024-04-15 21:39:46 +0200

    Switched to DevHost Address until further updates get launched on the production one

--- src/Constants/apiRoutes.js
+++ src/Constants/apiRoutes.js
@@ -1,12 +1,12 @@
 // API Host and Routes Grouper
 
-const hostAddress = "https://spire-treasury-backend.onrender.com"; // Production Backend Host Address
-// const devAddress = "http://localhost:5000"; // Development Backend Host Address
+// const hostAddress = "https://spire-treasury-backend.onrender.com"; // Production Backend Host Address
+const devAddress = "http://localhost:5000"; // Development Backend Host Address
 
 const apiRoutes = {
-  createUser: hostAddress + "/users/signup",
-  loginUser: hostAddress + "/users/login",
-  getSingleUser: hostAddress + "/users/user",
+  createUser: devAddress + "/users/signup",
+  loginUser: devAddress + "/users/login",
+  getSingleUser: devAddress + "/users/user",
 };
 
 export default apiRoutes;
