11fd9bbe29079c60315cda638e23589b0a345652
Nourelden Rihan <noureldenrihan@gmail.com>
2024-04-18 16:31:21 +0200

    Added a new SVG

--- src/Constants/Icons.js
+++ src/Constants/Icons.js
@@ -3,6 +3,7 @@
 import { ReactComponent as closedEye } from "../SVGs/closedEye.svg";
 import { ReactComponent as eye } from "../SVGs/eye.svg";
 import { ReactComponent as logout } from "../SVGs/logout.svg";
+import { ReactComponent as settings } from "../SVGs/settings.svg";
 
 // Assign for easier single imports
 
@@ -10,5 +11,6 @@ export const UnifiedSVGSize = 24;
 export const ClosedEye = closedEye;
 export const Eye = eye;
 export const LogoutIcon = logout;
+export const Settings = settings;
 
 //TODO Setup Global Basic SVG Icons styles
